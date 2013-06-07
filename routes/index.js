0a7cd6c9681b478e97c3aee05d824146e7d3b199
Jautenim <e6990620@gmail.com>
2013-06-07 21:46:08 +0200

    Wishes are now saved to mongoDB and displayed to left panel on page reload (Quick and dirty implementation)

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,24 @@
 /*
  * GET home page.
  */
-
+var mongo = require('mongodb');
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+  var db = mongo.Db.connect('mongodb://localhost:27017/wish', {auto_reconnect: true}, function(err, db) {
+    if (err)
+      throw err;
+
+    db.collection('wishes', function(err, collection) {
+      if (err)
+        throw err;
+
+      collection.find().toArray(function (err, items) {
+        if (err)
+          throw err;
+
+        console.log("items fetched to prepopulate page: " + JSON.stringify(items));
+        res.render('index', { wishes: items });
+      }); 
+    });
+  });
 };
--- routes/wish.js
+++ routes/wish.js
@@ -1,3 +1,48 @@
+var mongo = require('mongodb');
+var db = mongo.Db.connect('mongodb://localhost:27017/wish', {auto_reconnect: true}, function(err, db) {
+    if (err)
+      throw err;
+
+    console.log("Got handler to database, yay!");
+    /* // create a test collection in the database
+    db.createCollection('test', function(err, test) {
+      if (err)
+        throw err;
+      // insert a dummy document into the test collection          
+      test.insert({'name':'admin', 'pass':'admin'});
+
+      app.listen(3000); 
+      console.log('App listening on port 3000');
+    }); */
+  });
+
 exports.add = function (req, res) {
-  res.send('Echoing data back: ' + req.body.tag + ' - ' + req.body.price);
+  // TODO: Validate input
+
+  // TODO: db as a global variable decoupled from .add()
+  var db = mongo.Db.connect('mongodb://localhost:27017/wish', {auto_reconnect: true}, function(err, db) {
+    if (err)
+      throw err;
+
+    console.log("Got handler to database, yay!");
+    db.collection('wishes', function(err, collection) {
+      if (err)
+        throw err;
+      console.log("seems we're in...");
+      collection.insert({tag: req.body.tag, price: req.body.price, date: new Date()}, function(err, result) {
+        console.log("Insert done! Just out of curiosity: " + JSON.stringify(result));
+      });
+    });
+    /* // create a test collection in the database
+    db.createCollection('test', function(err, test) {
+      if (err)
+        throw err;
+      // insert a dummy document into the test collection          
+      test.insert({'name':'admin', 'pass':'admin'});
+
+      app.listen(3000); 
+      console.log('App listening on port 3000');
+    }); */
+  });
+  
 };
