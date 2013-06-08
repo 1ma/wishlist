b00dc65b25652f2cdc8120575902efb05559a669
Jautenim <e6990620@gmail.com>
2013-06-08 22:26:05 +0200

    Wishes filtered appropiately

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
 
+  // TODO: Handle possible errors
   $.get('/wishes', function(wishes) {
-    // TODO: Handle possible errors
 
-    var makePrintFunction = function(selector) {
+    var lessThanAMonth = function(elem) {
+      var oneMonth = 31*24*60*60*1000;
+      return new Date().getTime() - new Date(elem.date).getTime() < oneMonth;
+    }
+
+    var printWishesAt = function(selector) {
       return function(element) {
-        selector.append('<li>' + element.tag + ' ' + element.price + ' ' + element.date + '</li>');
+        selector.append('<li>' + element.tag + ' ' + element.price + ' ' + new Date(element.date).toDateString() + '</li>');
       };
     }
-    
-    var pending_wishes = _.select(wishes, function(wish) {
-      return wish.price < 1000;
-    });
-    _.each(pending_wishes, makePrintFunction($('#pending_list')));
+
+    var pending_wishes = _.select(wishes, lessThanAMonth);
+    _.each(pending_wishes, printWishesAt($('#pending_list')));
 
     var approved_wishes = _.difference(wishes, pending_wishes);
-    _.each(approved_wishes, makePrintFunction($('#approved_list')));
-
+    _.each(approved_wishes, printWishesAt($('#approved_list')));
   });
 });
 
 $("#submitButton").click(function() {
+  
   var tag = $('#tag').val();
   var price = $('#price').val();
+  
+  // TODO: Handle possible errors
   $.post('/wishes', {"tag": tag, "price": price}, function() {
-    // TODO: Handle possible errors
-
+    
     $('#pending_wishes_list').append('<li>' + tag + ' ' + price + '</li>');
   });
 });
